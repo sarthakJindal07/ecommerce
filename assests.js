0c8ebd2ff707702af5cd9fa06c8b13396a5ca3d3
sarthakJindal07 <91410321+sarthakJindal07@users.noreply.github.com>
2024-10-11 15:30:58 +0530

    Add files via upload

---
+++ assests.js
@@ -0,0 +1,370 @@
+import bed_img18_1 from './bed_img18_1.jpg'
+import bed_img18_2 from './bed_img18_2.jpg'
+import bed_img18_3 from './bed_img18_3.jpg'
+import bed_img18_4 from './bed_img18_4.jpg'
+import bed_img19_1 from './bed_img19_1.jpg'
+import bed_img19_2 from './bed_img19_2.jpg'
+import bed_img19_3 from './bed_img19_3.jpg'
+import bed_img20_1 from './bed_img20_1.jpg'
+import bed_img20_2 from './bed_img20_2.jpg'
+import bed_img20_3 from './bed_img20_3.jpg'
+import bed_img20_4 from './bed_img20_4.jpg'
+import bed_img21_1 from './bed_img21_1.jpg'
+import bed_img21_2 from './bed_img21_2.jpg'
+import bed_img21_3 from './bed_img21_3.jpg'
+import bed_img21_4 from './bed_img21_4.jpg'
+import bed_img22_1 from './bed_img22_1.jpg'
+import bed_img22_2 from './bed_img22_2.png'
+import bed_img22_3 from './bed_img22_3.jpg'
+import bednon_img23_1 from './bednon_img23_1.jpg'
+import bednon_img23_2 from './bednon_img23_2.jpg'
+import bednon_img23_3 from './bednon_img23_3.jpg'
+import bednon_img23_4 from './bednon_img23_4.jpg'
+import bednon_img24_1 from './bednon_img24_1.jpg'
+import bednon_img24_2 from './bednon_img24_2.jpg'
+import bednon_img24_3 from './bednon_img24_3.jpg'
+import bednon_img24_4 from './bednon_img24_4.jpg'
+import bednon_img25_1 from './bednon_img25_1.jpg'
+import bednon_img25_2 from './bednon_img25_2.jpg'
+import bednon_img25_3 from './bednon_img25_3.jpg'
+import matt_img26_1 from './matt_img26_1.jpg'
+import matt_img26_2 from './matt_img26_2.png'
+import matt_img26_3 from './matt_img26_3.jpg'
+import matt_img27_1 from './matt_img26_1.jpg'
+import matt_img27_2 from './matt_img26_2.png'
+import matt_img27_3 from './matt_img26_3.jpg'
+import matt_img28_1 from './matt_img26_1.jpg'
+import matt_img28_2 from './matt_img26_2.png'
+import matt_img28_3 from './matt_img26_3.jpg'
+import matt_img29_1 from './matt_img26_1.jpg'
+import matt_img29_2 from './matt_img26_2.png'
+import matt_img29_3 from './matt_img26_3.jpg'
+import dhurri_img61_1 from './dhurri_img61_1.jpg'
+import dhurri_img61_2 from './dhurri_img61_2.png'
+import dhurri_img61_3 from './dhurri_img61_3.jpg'
+import dhurri_img62_1 from './dhurri_img62_1.jpg'
+import dhurri_img62_2 from './dhurri_img62_2.png'
+import dhurri_img62_3 from './dhurri_img62_3.jpg'
+import dhurri60_1 from './dhurri60_1.jpg'
+import dhurri60_2 from './dhurri60_2.jpg'
+import dhurri60_3 from './dhurri60_3.jpg'
+import carpet_img63_1 from './carpet_img63_1.jpg'
+import carpet_img63_2 from './carpet_img63_2.jpg'
+import carpet_img63_3 from './carpet_img63_3.jpg'
+import carpet_img64_1 from './carpet_img64_1.jpg'
+import carpet_img64_2 from './carpet_img64_2.jpg'
+import carpet_img64_3 from './carpet_img64_3.jpg'
+import carpet_img65_1 from './carpet_img65_1.jpg'
+import carpet_img65_2 from './carpet_img65_2.jpg'
+import carpet_img65_3 from './carpet_img65_3.jpg'
+import walldecor_img66_1 from './walldecor_img66_1.jpg'
+import walldecor_img66_2 from './walldecor_img66_2.jpg'
+import walldecor_img66_3 from './walldecor_img66_3.jpg'
+import walldecor_img67_1 from './walldecor_img67_1.jpg'
+import walldecor_img67_2 from './walldecor_img67_2.jpg'
+import walldecor_img67_3 from './walldecor_img67_3.jpg'
+import walldecor_img68_1 from './walldecor_img68_1.jpg'
+import walldecor_img68_2 from './walldecor_img68_2.jpg'
+import walldecor_img68_3 from './walldecor_img68_3.jpg'
+import wallaccent_img69_1 from './wallaccent_img69_1.jpg'
+import wallaccent_img69_2 from './wallaccent_img69_2.jpg'
+import wallaccent_img70_1 from './wallaccent_img70_1.jpg'
+import wallaccent_img70_2 from './wallaccent_img70_2.jpg'
+import wallaccent_img71_1 from './wallaccent_img71_1.jpg'
+import wallaccent_img71_2 from './wallaccent_img71_2.jpg'
+import wallaccent_img71_3 from './wallaccent_img71_3.jpg'
+
+
+
+const products = [
+                                            //allbeds
+{                 
+ id : "aaaaa1",
+ name: "Knox Engineered Wood Queen Size Non Storage Bed in Exotic Teak Finish",
+ discpritiom : "Crafted with meticulous attention to detail, this furniture piece embodies both elegance and durability. Its timeless design seamlessly blends with any decor, enhancing the aesthetic appeal of your space. Elevate your home with A Globia Creations commitment to excellence in every aspect of furniture craftsmanship.",
+ price : "₹26,146.00 (Incl. of all taxes)",
+ image : [bed_img18_1,bed_img18_2,bed_img18_3,bed_img18_4],
+ category:"BedsandMattresses",
+ subcategory:"Queen and King size Beds",
+ dimentions:"85.0 cm H x 159.4 cm W x 218.7 cm L",
+ netquantity :"1N"
+},
+
+{
+    id: "aaaaa2",
+    name:"Tyra Storage Bed With Theramedic Coir & Foam Mattress",
+    discpritiom:"Crafted with meticulous attention to detail, this furniture piece embodies both elegance and durability. Its timeless design seamlessly blends with any decor, enhancing the aesthetic appeal of your space. Elevate your home with A Globia Creations commitment to excellence in every aspect of furniture craftsmanship.",
+    price:"₹82,298.00 (Incl. of all taxes",
+    image:[bed_img19_1,bed_img19_2,bed_img19_3],
+    category:"BedsandMattresses",
+    subcategory:"Queen and King size Beds",
+    dimentions:"104.9 cm H x 190.5 cm W x 224.0 cm L",
+    netquantity:"1N"
+},
+
+{
+    id: "aaaaa3",
+    name:"Lavish Engineered Wood Queen Size Box Storage Upholstered Bed in Walnut Finish",
+    discpritiom:"Crafted with meticulous attention to detail, this furniture piece embodies both elegance and durability. Its timeless design seamlessly blends with any decor, enhancing the aesthetic appeal of your space. Elevate your home with DF2H commitment to excellence in every aspect of furniture craftsmanship",
+    price:"₹82,298.00 (Incl. of all taxes",
+    image:[bed_img20_1,bed_img20_2,bed_img20_3,bed_img20_4],
+    category:"BedsandMattresses",
+    subcategory:"Queen and King size Beds",
+    dimentions:"90.0 cm H x 204.5 cm W x 157.4 cm L",
+    netquantity:"1N"
+},
+
+{
+    id: "aaaaa4",
+    name:"Tyra Storage Bed With Theramedic Coir & Foam Mattress",
+    discpritiom:"Crafted with meticulous attention to detail, this furniture piece embodies both elegance and durability. Its timeless design seamlessly blends with any decor, enhancing the aesthetic appeal of your space. Elevate your home with DF2H commitment to excellence in every aspect of furniture craftsmanship",
+    price:"₹82,298.00 (Incl. of all taxes",
+    image:[bed_img21_1,bed_img21_2,bed_img21_3,bed_img21_4],
+    category:"BedsandMattresses",
+    subcategory:"Queen and King size Beds",
+    dimentions:"88.0 cm H x 203.0 cm W x 156.4 cm LL",
+    netquantity:"1N"
+},
+
+{
+    id: "aaaaa5",
+    name:"Aruba Engineered Wood Queen Size Hydraulic Storage Bed in Classic Walnut Finish",
+    discpritiom:" Elevate your home with DF2H commitment to excellence in every aspect of furniture craftsmanship",
+    price:"₹14,839.00 (Incl. of all taxes",
+    image:[bed_img22_1,bed_img22_2,bed_img22_3,bed_img22_3],
+    category:"BedsandMattresses",
+    subcategory:"Queen and King size Beds",
+    dimentions:"79.0 cm H x 125.4 cm W x 203.0 cm L",
+    netquantity:"1N"
+},
+
+                                  //non storage beds
+{
+    id: "aaaaa6",
+    name:"Axel Engineered Wood Queen Size Non Storage Bed in Frosty White Finish",
+    discpritiom:" Elevate your home with DF2H commitment to excellence in every aspect of furniture craftsmanship",
+    price:"₹29,143.00 (Incl. of all taxes",
+    image:[bednon_img23_1,bednon_img23_2,bednon_img23_3,bednon_img23_4],
+    category:"BedsandMattresses",
+    subcategory:"Non-Storage Beds",
+    dimentions:"95.0 cm H x 164.0 cm W x 204.5 cm L",
+    netquantity:"1N"
+},
+{
+    id: "aaaaa7",
+    name:"Weaver Metal Queen Size Bed in Black Finish",
+    discpritiom:" Elevate your home with DF2H commitment to excellence in every aspect of furniture craftsmanship",
+    price:"₹15,899.00 (Incl. of all taxes",
+    image:[bednon_img24_1,bednon_img24_2,bednon_img24_3,bednon_img24_4],
+    category:"BedsandMattresses",
+    subcategory:"Non-Storage Beds",
+    dimentions:"83.0 cm H x 206.0 cm W x 159.3 cm L",
+    netquantity:"1N"
+},
+{
+    id: "aaaaa8",
+    name:"Belize Engineered Wood Queen Size Upholstered Bed in Finish",
+    discpritiom:" Elevate your home with DF2H commitment to excellence in every aspect of furniture craftsmanship",
+    price:"₹44,999.00 (Incl. of all taxes",
+    image:[bednon_img25_1,bednon_img25_2,bednon_img25_3],
+    category:"BedsandMattresses",
+    subcategory:"Non-Storage Beds",
+    dimentions:"119.0 cm H x 247.6 cm W x 165.8 cm L",
+    netquantity:"1N"
+},
+
+                                                //mattresses
+{
+    id: "aaaaa9",
+    name:"Magic Coir Mattress - King Size",
+    discpritiom:" A comfortable mattress that puts an all day stress to rest. The three layer material of the mattress adjust with the surroundings and keeps the user comfortable in all conditions.",
+    price:"₹18,929.00 (Incl. of all taxes",
+    image:[matt_img26_1,matt_img26_2,matt_img26_3],
+    category:"BedsandMattresses",
+    subcategory:"mattresses",
+    dimentions:"15.2 cm H x 182.9 cm W x 198.1 cm L",
+    netquantity:"1N"
+},
+{
+    id: "aaaaa10",
+    name:"Orthopedic Dual Comfort - Hard & Soft High Resilience Foam Mattress - Queen Size",
+    discpritiom:" Elevate your home with DF2H commitment to excellence in every aspect of furniture craftsmanship",
+    price:"₹28,799.00 (Incl. of all taxes",
+    image:[matt_img27_1,matt_img27_2,matt_img27_3],
+    category:"BedsandMattresses",
+    subcategory:"mattresses",
+    dimentions:"20.3 cm H x 152.4 cm W x 198.1 cm L",
+    netquantity:"1N"
+},
+{
+    id: "aaaaa11",
+    name:"Theramedic Coir & Foam Mattress",
+    discpritiom:"SLEEP SPA DUAL COMFORT HR FOAM MATTRESS is all what you need for a sound sleep. A quality product offered from the house of COIRFIT MATTRESS. COIRFIT is a reputed brand in the market today. It has a legacy of more than 30 years in mattress business.This dual comfort mattress or in much simpler words as we can say this one side firm and other side soft mattress is designed to cater to every individual's personal sleeping needs.It is a both side usable mattress and one can easily choose between the two sides of the mattress for sleeping as per his comfort needs of sleeping on a firm or or a soft surface.It comprises of IMPORTED HR foam, which provides best in class durability and orthopedic features to this mattress.This mattress is designed to relieve back pain",
+    price:"₹30,199.00 (Incl. of all taxes",
+    image:[matt_img28_1,matt_img28_2,matt_img28_3],
+    category:"BedsandMattresses",
+    subcategory:"matteresses",
+    dimentions:"15.2 cm H x 152.4 cm W x 198.1 cm L",
+    netquantity:"1N"
+},
+{
+    id: "aaaaa12",
+    name:"Premium Orthopedic Memory Foam With Cooling Gel Memory Foam Mattress",
+    discpritiom:" .This mattress is designed to relieve back pain.All the five major factors essential for a good mattress are present in this mattress. first is Durability – higher foam density implies a longer lifetime",
+    price:"₹32,449.00 (Incl. of all taxes",
+    image:[matt_img29_1,matt_img29_2,matt_img29_3],
+    category:"BedsandMattresses",
+    subcategory:"matteresses",
+    dimentions:"15.2 cm H x 152.4 cm W x 198.1 cm L",
+    netquantity:"1N"
+},
+
+
+ //carpets
+
+{
+    id: "aaaaa13",
+    name:"Grey Solids Hand Tufted Wool 3 X 5 Feet Carpet",
+    discpritiom:"Exhibiting nature's beauty, the carpets are sure to fetch you complements from your guests. These Solid Hand-Tufted Wool carpets look beautiful and revive your tired feet each time you take a walk on them. Made of fine-quality materials, these durable carpets are easy to clean and maintain.",
+    price:"₹10,598.00 (Incl. of all taxes)",
+    image:[carpet_img63_1,carpet_img63_2,carpet_img63_3],
+    category:"Carpets",
+    subcategory:"carpet",
+    dimentions:"91.4 cm H x 152.4 cm W",
+    netquantity:"1N"
+},                                               
+{
+    id: "aaaaa14",
+    name:"Grey Abstract Hand Tufted Wool 3 X 5 Feet Carpet",
+    discpritiom:"Brown and Beige Abstract Hand-Tufted Wool carpets are graceful and refined, often characterized by the use of turnings and motifs to present an elegant appearance.",
+    price:"₹10,598.00 (Incl. of all taxes",
+    image:[carpet_img64_1,carpet_img64_2,carpet_img64_3],
+    category:"Carpets",
+    subcategory:"carpet",
+    dimentions:"91.4 cm H x 152.4 cm W",
+    netquantity:"1N"
+},                                               
+{
+    id: "aaaaa15",
+    name:"Gold Abstract Hand Tufted Wool 3 X 5 Feet Carpet",
+    discpritiom:"Modern Style Hand Tufted Carpet is graceful and refined, often characterized by the use of turnings and motifs to present an elegant appearance.",
+    price:"₹10,598.00 (Incl. of all taxes",
+    image:[carpet_img65_1,carpet_img65_2,carpet_img65_3],
+    category:"Carpets",
+    subcategory:"carpet",
+    dimentions:"91.4 cm H x 152.4 cm W",
+    netquantity:"1N"
+},                                               
+
+
+ //dhurris
+
+ {
+    id: "aaaaa16",
+    name:"Tia Multicoloured Traditional Hand Tufted Cotton Dhurrie",
+    discpritiom:"Modern Style Hand Tufted Carpet is graceful and refined, often characterized by the use of turnings and motifs to present an elegant appearance.",
+    price:"₹3,598.00 (Incl. of all taxes",
+    image:[dhurri_img62_1,dhurri_img62_2,dhurri_img62_3],
+    category:"Carpets",
+    subcategory:"dhurries",
+    dimentions:"120.0 cm W x 180.0 cm L",
+    netquantity:"1N"
+},   
+ {
+    id: "aaaaa17",
+    name:"Jude Multicoloured Traditional Hand Tufted Cotton Dhurrie",
+    discpritiom:"Modern Style Hand Tufted Carpet is graceful and refined, often characterized by the use of turnings and motifs to present an elegant appearance.",
+    price:"₹5,298.00 (Incl. of all taxes",
+    image:[dhurri_img61_1,dhurri_img61_2,dhurri_img61_3],
+    category:"Carpets",
+    subcategory:"dhurries",
+    dimentions:"91.4 cm H x 152.4 cm W",
+    netquantity:"1N"
+},   
+ {
+    id: "aaaaa18",
+    name:"Hanna Red Cotton Dhurrie",
+    discpritiom:"Modern Style Hand Tufted Carpet is graceful and refined, often characterized by the use of turnings and motifs to present an elegant appearance.",
+    price:"₹6,098.00 (Incl. of all taxes",
+    image:[dhurri60_1,dhurri60_2,dhurri60_3],
+    category:"Carpets",
+    subcategory:"dhurries",
+    dimentions:"1800.0 cm W x 1300.0 cm L",
+    netquantity:"1N"
+}, 
+
+
+//walldecors
+            //wallarts
+{
+    id: "aaaaa19",
+    name:"Grey Canvas Wall Art  ",
+    discpritiom:"High definition picture photo prints on canvas with vivid color on thick high quality canvas to create the look and feel of the original nature art and masterpiece.",
+    price:"₹5,998.00 (Incl. of all taxes",
+    image:[walldecor_img66_1,walldecor_img66_2,walldecor_img66_3],
+    category:"Wall Decor",
+    subcategory:"Wall art",
+    dimentions:"88.9 cm H x 50.8 cm W x 12.7 cm L",
+    netquantity:"1N"
+}, 
+{
+    id: "aaaaa20",
+    name:"White Canvas Wall Art  ",
+    discpritiom:"High definition picture photo prints on canvas with vivid color on thick high quality canvas to create the look and feel of the original nature art and masterpiece.",
+    price:"₹7,998.00 (Incl. of all taxes",
+    image:[walldecor_img67_1,walldecor_img67_2,walldecor_img67_3],
+    category:"Wall Decor",
+    subcategory:"Wall art",
+    dimentions:"330.2 cm H x 193.0 cm W x 1.8 cm L",
+    netquantity:"1N"
+}, 
+{
+    id: "aaaaa21",
+    name:"Grey Synthetic Fiber Wall Art  ",
+    discpritiom:"High definition picture photo prints on canvas with vivid color on thick high quality canvas to create the look and feel of the original nature art and masterpiece.",
+    price:"₹3,998.00 (Incl. of all taxes",
+    image:[walldecor_img68_1,walldecor_img68_2,walldecor_img68_3],
+    category:"Wall Decor",
+    subcategory:"Wall art",
+    dimentions:"55.9 cm H x 88.9 cm W x 2.5 cm L",
+    netquantity:"1N"
+}, 
+
+            //wallaccents
+{
+    id: "aaaaa22",
+    name:"White Metal Wall Accent  ",
+    discpritiom:"Introducing a standout home decor piece from Handicrafts Town crafted with the finest materials and meticulous attention to detail. This product embodies elegance and sophistication, adding a touch of luxury to your living space. Its timeless design seamlessly complements any decor style, making it a versatile and enduring addition to your home.",
+    price:"₹8,998.00 (Incl. of all taxes",
+    image:[wallaccent_img69_1,wallaccent_img69_2],
+    category:"Wall Decor",
+    subcategory:"Wall Accents",
+    dimentions:"61.0 cm H x 139.7 cm W x 5.1 cm L",
+    netquantity:"1N"
+}, 
+{
+    id: "aaaaa23",
+    name:"Grey Metal Wall Accent  ",
+    discpritiom:"Introducing a standout home decor piece from Handicrafts Town crafted with the finest materials and meticulous attention to detail. This product embodies elegance and sophistication, adding a touch of luxury to your living space. Its timeless design seamlessly complements any decor style, making it a versatile and enduring addition to your home.",
+    price:"₹4,898.00 (Incl. of all taxes",
+    image:[wallaccent_img70_1,wallaccent_img70_2],
+    category:"Wall Decor",
+    subcategory:"Wall Accents",
+    dimentions:"61.0 cm H x 124.5 cm W x 5.1 cm L",
+    netquantity:"1N"
+}, 
+{
+    id: "aaaaa24",
+    name:"Orange Metal Wall Accent ",
+    discpritiom:"Introducing a standout home decor piece from Handicrafts Town crafted with the finest materials and meticulous attention to detail. This product embodies elegance and sophistication, adding a touch of luxury to your living space. Its timeless design seamlessly complements any decor style, making it a versatile and enduring addition to your home.",
+    price:"₹7,998.00 (Incl. of all taxes",
+    image:[wallaccent_img71_1,wallaccent_img71_2,wallaccent_img71_3],
+    category:"Wall Decor",
+    subcategory:"Wall Accents",
+    dimentions:"76.2 cm H x 109.2 cm W x 5.1 cm L",
+    netquantity:"1N"
+}, 
+
+                                                
+
+]
